0609ca0b4e38f4cfc979e558790cdfa838243966
Tyler Golden <tyler@tylergolden.net>
2015-08-26 19:26:12 -0400

    Adding twig for templating

--- web/index.php
+++ web/index.php
@@ -3,6 +3,17 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
-$app['debug'] = true;
-$app->run();
 
+$app['debug'] = true;
+
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views',
+));
+
+$app->get('/hello/{name}', function ($name) use ($app) {
+    return $app['twig']->render('hello.twig', array(
+        'name' => $name,
+    ));
+});
+
+$app->run();
