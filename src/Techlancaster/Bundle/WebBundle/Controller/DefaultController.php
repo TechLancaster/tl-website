a3919a9fdf21b8e13db9324593566d0eee780cdd
Tyler Golden <tyler@tylergolden.net>
2016-01-04 19:28:53 -0500

    Moved events to database storage, added controller to return all events as starting point for API.

--- src/Techlancaster/Bundle/WebBundle/Controller/DefaultController.php
+++ src/Techlancaster/Bundle/WebBundle/Controller/DefaultController.php
@@ -11,38 +11,33 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        if (file_exists("events.json") &&
-            ($file = file_get_contents("events.json")) !== false
-        ) {
-            $json = json_decode($file, true);
-        } else {
-            $json = array();
-        }
+        $em = $this->get('doctrine.orm.entity_manager');
+        $events = $em->getRepository('TechlancasterWebBundle:Event')->findBy([],['start'=>'ASC']);
 
         // Modify the data to fit into the template
-        foreach ($json as &$event) {
-            /* the title should handle a max of ~50 characters to accomodate 3 lines
+        foreach ($events as &$event) {
+            /* the title should handle a max of ~50 characters to accommodate 3 lines
                 at it's smallest width (browser width of 992px).
                 truncate summary at last full word */
-            if (strlen($event['summary']) > 47) {
-                $event['summary'] = wordwrap($event['summary'], 47);
-                $event['summary'] = substr($event['summary'], 0, strpos($event['summary'], "\n")) . '...';
+            if (strlen($event->getSummary()) > 47) {
+                $event->setSummary(wordwrap($event->getSummary(), 47));
+                $event->setSummary(substr($event->getSummary(), 0, strpos($event->getSummary(), "\n")) . '...');
             }
 
             // make links in description clickable
             $pattern_url = '/((?:(?:http|ftp)s?:\/\/)?[a-z0-9\-\.]+\.[a-z]{2,5}(?:\/\S*)?)/i';
             $url_replacement = '<a href="$1">$1</a> ';
-            $event['description'] = preg_replace($pattern_url, $url_replacement, $event['description']);
+            $event->setDescription(preg_replace($pattern_url, $url_replacement, $event->getDescription()));
             // replace newlines with <br> tag
-            $event['description'] = nl2br($event['description']);
+            $event->setDescription(nl2br($event->getDescription()));
 
             /* location needs link and truncation
                (for one line, cap at ~30 characters) */
             // init short_location
-            $short_location = $event['location'];
+            $short_location = $event->getLocation();
             // truncate short_location at the first comma
             if (strpos($short_location, ',') !== false) {
-                $short_location = substr($event['location'], 0, strpos($event['location'], ','));
+                $short_location = substr($event->getLocation(), 0, strpos($event->getLocation(), ','));
             }
             // truncate short_location after the 30th character respecting whole words
             if (strlen($short_location) > 30) {
@@ -52,14 +47,19 @@ class DefaultController extends Controller
                 $short_location = preg_replace('/[^a-z\d]+$/i', '', $short_location);
             }
             // create location as link with short text
-            $event['location'] = '<a href="https://www.google.com/maps/?q=' . $event['location'] . '" >' . $short_location . '</a>';
+            $event->setLocation(
+                '<a href="https://www.google.com/maps/?q=' . $event->getLocation() . '" >' . $short_location . '</a>'
+            );
         }
+        $em->clear();
 
-        return $this->render('TechlancasterWebBundle:Default:index.html.twig', array('events' => $json));
+        return $this->render('TechlancasterWebBundle:Default:index.html.twig', array('events' => $events));
     }
 
     public function fetchAction()
     {
+        $em = $this->getDoctrine()->getEntityManager();
+
         // TODO move to a cron job
         // TODO move google credentials to symfony parameters file
         $configDirectories = array(__DIR__.'/../Resources/config');
@@ -103,6 +103,11 @@ class DefaultController extends Controller
         $results = $service->events->listEvents($calendarId, $optParams);
         $events = array();
 
+        $connection = $em->getConnection();
+        $platform   = $connection->getDatabasePlatform();
+
+        $connection->executeUpdate($platform->getTruncateTableSQL('event', false));
+
         /**
          * @var $googleEvent Google_Service_Calendar_Event
          */
@@ -111,20 +116,17 @@ class DefaultController extends Controller
             $event->setId($googleEvent->getId());
             $event->setDescription($googleEvent->getDescription());
             $event->setLocation($googleEvent->getLocation());
-            $event->setStart($googleEvent->getStart());
-            $event->setEnd($googleEvent->getEnd());
+            $event->setStart(new \DateTime($googleEvent->getStart()->dateTime));
+            $event->setEnd(new \DateTime($googleEvent->getEnd()->dateTime));
             $event->setSummary($googleEvent->getSummary());
+            $em->persist($event);
             $events[] = $event;
         }
+        $em->flush();
 
-        // Write the events to a file
-        $json = json_encode($events);
-        $handler = fopen("events.json", 'w')
-        or die("Error opening output file");
-        fwrite($handler, $json);
-        fclose($handler);
-
-        return $this->render('TechlancasterWebBundle:Default:calendar.html.twig', array('message'=>$json));
+        return $this->render('TechlancasterWebBundle:Default:calendar.html.twig', array(
+            'message'=>json_encode($events))
+        );
     }
 
     public function meetupAction()
