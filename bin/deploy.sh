e8327d95811a8e9d7796c4854637634b7fa8446e
Tyler Golden <tyler@tylergolden.net>
2016-07-12 09:42:14 -0400

    Project doesn't have migrations, correcting deploy script.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 php composer.phar install
-php app/console doctrine:migrations:status
+php app/console doctrine:schema:validate
 php app/console cache:clear --env=prod
 chown -R www-data app/cache app/logs
